5d684388dfd873ab2bb7526de4b263aa3f3db5bd
Maria <mcamfer272a@ieslacampina.es>
2024-02-06 14:30:38 +0100

    borracion de h1

--- index.php
+++ index.php
@@ -8,7 +8,5 @@
 <body>
     <p>Hola</p>
     <h1>Esto lo hago desde local me llammo Carlos</h1>
-    <p>Hola me llamo María y lo subo desde local</p>
-    <p>Nuevo cambio carlos</p>
 </body>
 </html>
