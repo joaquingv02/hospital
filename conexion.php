64c5fbee56a6e17d9cfb2b02230a6f427be5e000
Juanito <jgalvel524a@ieslacampina.es>
2024-02-13 13:44:11 +0100

    Primeros cambios y bootstrap

---
+++ conexion.php
@@ -0,0 +1,11 @@
+<?php
+//Nueva conexión de la clase mysql
+//Parámetros: ubicación del servidor, nombre de usuario, contraseña, base de datos
+$mysqli = new mysqli("localhost", "root", "", "hospital");
+if ($mysqli->connect_errno) {
+    echo "Fallo al conectar a MySql: (", $mysqli->connect_errno, ")", $mysqli->connect_errno;
+}
+// else{
+//         echo "Todo bien, amigo";
+//     }
+?>
